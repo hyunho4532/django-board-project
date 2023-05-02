533f13bbd3320c303f16ccb2d9c3bcaa4d6602d5
hyunho4562 <kyounguk192@gmail.com>
2023-05-02 22:54:10 +0900

    2023-05-02 질문 상세 페이지 구현

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -15,3 +15,15 @@ def index(request):
     context = {'question_list': question_list}
 
     return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request, question_id):
+
+    """
+    pybo 내용 출력
+    """
+
+    question = Question.objects.get(id=question_id)
+    context = {'question': question}
+
+    return render(request, 'pybo/question_detail.html', context)
