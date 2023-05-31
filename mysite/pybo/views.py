e6c0a6dec258333919c3d183952f1e4107978a47
hyunho4562 <kyounguk192@gmail.com>
2023-05-31 22:31:07 +0900

    2023-05-31 페이징  적용

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -13,8 +14,16 @@ def index(request):
     pybo 목록 출력
     """
 
+    page = request.GET.get('page', '1') # 페이지
+
+    # 조회
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+
+    # 페이징 처리
+    paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여 주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
 
     return render(request, 'pybo/question_list.html', context)
 
